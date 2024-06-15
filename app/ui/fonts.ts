ff837330de6d9e25042500c2534cdb14580ff2da
hyeyeonDev <hyeyeon.dev@gmail.com>
2024-06-15 22:35:18 +0900

    init project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,18 @@
+import { Inter, Lusitana, Noto_Sans_KR, Ubuntu } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const notoSansKr = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const ubuntu = Ubuntu({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
